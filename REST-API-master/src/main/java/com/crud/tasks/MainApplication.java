9abf042a4160d2fe6fe84cb0f5e18f99cdf48a50
Chojnacki <twojemail@example.com>
2024-04-26 02:14:15 +0200

    .

---
+++ REST-API-master/src/main/java/com/crud/tasks/MainApplication.java
@@ -0,0 +1,17 @@
+package com.crud.tasks;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@OpenAPIDefinition
+@SpringBootApplication
+public class MainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+	}
+
+}
---
+++ REST-API-master/src/main/java/com/crud/tasks/domain/TrelloListDto.java
@@ -0,0 +1,28 @@
+package com.crud.tasks.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TrelloListDto {
+    @JsonProperty("id")
+    private String id;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("closed")
+    private boolean isClosed;
+
+    public TrelloListDto(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
+
