efb2c19297f398828d38b4ba7381bef1bcbf65e5
Chojnacki <twojemail@example.com>
2024-04-29 16:56:19 +0200

    .

---
+++ REST-API-master/src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,58 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/v1/tasks")
+@RequiredArgsConstructor
+public class TaskController {
+
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+    @GetMapping
+    public List<TaskDto> getTasks() {
+        List<Task> tasks = service.getAllTasks();
+        return ResponseEntity.ok (taskMapper.mapToTaskDtoList(tasks)).getBody();
+    }
+
+
+    @GetMapping(value = "{taskId}")
+    public ResponseEntity<TaskDto> getTask(@PathVariable Long taskId) throws TaskNotFoundException {
+            return ResponseEntity.ok(taskMapper.mapToTaskDto(
+                    service.getTask(taskId)));
+    }
+
+    @DeleteMapping(value = "{taskId}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
+        service.deleteById(taskId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+
+    }
+    @PutMapping
+    public ResponseEntity<TaskDto> updateTask(@RequestBody TaskDto taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        Task savedTask = service.saveTask(task);
+        return ResponseEntity.ok(taskMapper.mapToTaskDto(savedTask));
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createTask(@RequestBody TaskDto taskDto) {
+        Task task = taskMapper.mapToTask(taskDto);
+        service.saveTask(task);
+        return ResponseEntity.ok().build();
+    }
+}
